371747f599209df18e3fb9e79c5072d23822c782
EmmaWartena <146272536+EmmaWartena@users.noreply.github.com>
2024-09-21 17:48:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.write(
  '''In de eerste optie is er gekeken naar het aantal bandenwissels van het afgelopen jaar en hier is een waarde uitgekomen. Deze waarde geeft aan hoeveel bandenwissels er plaatsvinden per week gemiddeld. 
          Er is hier dus niet meegenomen dat er de waardes misschien niet hetzelfde zijn als je een analyse wil van een speccifieke methode.''')
 
-st.markdown("
+st.markdown('''
             Voordelen:
             - Wanneer het nummer van vluchten dit jaar gemiddeld genomen hetzelfde blijft vergeleken met vorrig jaar s dit een betrouwbare meting wanneer deze wordt uigevoerd over een langere periode;
             - Aangezien er vooral data bekend is van de zomerperiode (toen alle vliegtuigen aanwezig waren) pakt deze waarde hoogstwaarschijnlijk hoger uit dan de realiteit waardoor het niet 
@@ -21,5 +21,5 @@ st.markdown("
             - De analyse is niet betrouwbaar wanneer deze informatie moet bereken voor kleinere periodes, aangezien er geen factor wordt meegnoemen door invloeden van buitenaf. Denk hier bijvoorbeeld aan de 
             temperatuur op bestemming in de zomerperiodes, vluchten naar nieuwe bestemmingen, of andere factors die invloed kunnen hebben op de banden.
             - De analyse is ook minder betrouwbaar wanneer de flight planning er aankomend jaar anders uit zou zien als die van afgelopen jaar. Dit omdat de waarde een gemiddelde is, die is gebaseerd op het afgelopen jaar. 
-            Dit maakt de waarde minder betrouwbaar op een volgend jaar met andere omstandigheden. "
+            Dit maakt de waarde minder betrouwbaar op een volgend jaar met andere omstandigheden. '''
             )
