ce8f3780c8807206bb968396228f2c9b9d8bb10b
EmmaWartena <146272536+EmmaWartena@users.noreply.github.com>
2024-09-21 16:35:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
 
 st.title("Wheel analysis Boeing 737 MAX 9")
-st.write(
-    "Er zijn verschillende opties voor het bepalen van de voorraad van banden die nodig zijn voor het nieuwe jaar. Bij elke optie worden ook de voor- en nadelen besproken en op welke parameter de optie vooral is gebaseerd."
+st.write( "Er zijn verschillende opties voor het bepalen van de voorraad van banden die nodig zijn voor het nieuwe jaar. Bij elke optie worden ook de voor- en nadelen besproken en op welke parameter de optie vooral is gebaseerd."
