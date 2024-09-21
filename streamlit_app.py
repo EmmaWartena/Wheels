91f30a28b79d7c425f69bd7d1677897bdcd77622
EmmaWartena <146272536+EmmaWartena@users.noreply.github.com>
2024-09-21 17:00:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ import pandas as pd
 st.title("Wheel analysis Boeing 737 MAX 9")
 st.write( 
   "Er zijn verschillende opties voor het bepalen van de voorraad van banden die nodig zijn voor het nieuwe jaar. Bij elke optie worden ook de voor- en nadelen besproken en op welke parameter de optie vooral is gebaseerd.")
+st.header("Optie 1: Trent van bandenwissels doortrekken")
